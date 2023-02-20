9115d326e58717166c84d6a3183895fd641bea70
Ari Abendstern <ari.abendstern@gmx.com>
2023-02-20 16:41:20 +0000

    changed company name h1 to h2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ class Header extends HTMLElement {
         `<nav>            
          
     <ul>
-    <li><a href="index.html"><h1>Company Name</h1></a></li>
+    <li><a href="index.html"><h2>Company Name</h2></a></li>
     <li><a href="Contact.html">Contact</a></li>
     <li><a href="About.html">About</a></li>
     <li><a href="Greece.html">Greece</a></li>
