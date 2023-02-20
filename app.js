e76b5e3ff1df445bf7e78e0cf11ae3460c747c1f
Ari Abendstern <ari.abendstern@gmx.com>
2023-02-20 14:21:45 +0000

    Deleted superfluous pages, started on navbar template

---
+++ app.js
@@ -0,0 +1,15 @@
+const header = document.createElement('navbar');
+
+template.innerHTML = `
+<nav>
+<ul>
+<li><a href="Home.html"><h1>Company Name</h1></a></li>
+<li><a href="Contact.html">Contact</a></li>
+<li><a href="About.html">About</a></li>
+<li><a href="Greece.html">Greece</a></li>
+<li><a href="Japan.html">Japan</a></li>
+</ul> 
+</nav>
+`;
+
+document.body.appendChild(navbar.content);
