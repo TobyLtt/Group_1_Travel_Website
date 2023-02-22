a866ce1381030ef14feec95442d9d89f8607ce34
Toby Little <Toby_Little@gmail.com>
2023-02-22 14:20:31 +0000

    loads of shit

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ class Header extends HTMLElement {
         `<nav class="navbar navbar--header">            
          
     <ul>
-    <li class="navbar navbar--header navbar__title navbar__link"><a href="index.html"><h2>TRIPR</h2></a></li>
-    <li class="navbar navbar--header navbar--header__normal navbar__normal navbar__link"><a href="Contact.html">Contact</a></li>
-    <li class="navbar navbar--header navbar--header__normal navbar__normal navbar__link"><a href="About.html">About</a></li>
-    <li class="navbar navbar--header navbar--header__normal navbar__normal navbar__link"><a href="Greece.html">Greece</a></li>
-    <li class="navbar navbar--header navbar--header__normal navbar__normal navbar__link"><a href="Japan.html">Japan</a></li>
+    <li class="navbar navbar__title navbar__link"><a href="index.html"><h2>TRIPR</h2></a></li>
+    <li class="navbar navbar--header__normal navbar__normal navbar__link"><a href="Contact.html">Contact</a></li>
+    <li class="navbar navbar--header__normal navbar__normal navbar__link"><a href="About.html">About</a></li>
+    <li class="navbar navbar--header__normal navbar__normal navbar__link"><a href="Greece.html">Greece</a></li>
+    <li class="navbar navbar--header__normal navbar__normal navbar__link"><a href="Japan.html">Japan</a></li>
     </ul> 
          
 </nav>`
